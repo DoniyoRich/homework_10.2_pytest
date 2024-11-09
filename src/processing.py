813888eb0a2f471dfb24410f595fe25ce11d391b
Doniyor <donish1979@gmail.com>
2024-11-09 10:31:02 +0300

    hw finished but readme not yet

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,14 @@
-def filter_by_state(dicts_to_filter: list, state="EXECUTED": str) -> list:
-    pass
+def filter_by_state(dicts_to_filter: list, state: str = "EXECUTED") -> list:
+    """Функция возвращает список словарей по фильтру значения state."""
+    filtered_list = []
+    for dictionary in dicts_to_filter:
+        if dictionary["state"] == state:
+            filtered_list.append(dictionary)
+
+    return filtered_list
 
 
-def sort_by_date(dicts_to_sort: list, rev_sorted=False: bool) -> list :
-    pass
+def sort_by_date(dicts_to_sort: list, rev_sorted: bool = True) -> list:
+    """Функция возращает список словарей, отсортированный по дате."""
 
+    return sorted(dicts_to_sort, key=lambda x: x["date"], reverse=rev_sorted)
